1444034ad9362c3ca47c63313fbe9bf97f2ac161
valentin <valentindu64@hotmail.fr>
2015-03-18 22:54:32 +0100

    Begin of the process

--- script.py
+++ script.py
@@ -1,1 +1,36 @@
  
+#!/usr/bin/python
+import time
+import os
+import glob
+import _mysql
+
+os.system('modprobe w1-gpio')
+os.system('modprobe w1-therm')
+base_dir = '/sys/bus/w1/devices/'
+device_folder = glob.glob(base_dir + '28*')[0]
+device_file = device_folder + '/w1_slave'
+
+db=_mysql.connect(host= ‘’localhost’’,db=’’temperature’’, user= ‘’root’’, passwd=’’Oybocphideitevifoch0’’)
+db.query=("""CREATE TABLE IF NOT EXISTS temperature (id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY, date DATE NOT NULL DEFAULT '0000-00-00', heure TIME NOT NULL DEFAULT '00:00:00', temperature FLOAT)""")
+
+def read_temp_raw():
+	f = open(device_file, 'r')
+	lines = f.readlines()
+	f.close()
+	return lines
+
+def read_temp():
+	lines = read_temp_raw()
+	while lines[0].strip()[-3:] != 'YES':
+		time.sleep(0.2)
+		lines = read_temp_raw()
+	equals_pos = lines[1].find('t=')
+	if equals_pos != -1:
+		temp_string = lines[1][equals_pos+2:]
+		temp_c = float(temp_string) / 1000.0
+   return temp_c
+
+print(read_temp())
+db.query('INSERT INTO temperature VALUES ( null, CURDATE(), CURTIME(), ' + "%.2f" % temp_c')')
+		 
