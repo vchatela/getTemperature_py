6140407d97f2c3f5aab1bb749197f5aecb5bcf72
Valentin C <valentindu64@hotmail.fr>
2015-11-26 20:01:42 +0100

    Restart counter of val_remaining_refresh_time_file

--- android_heater.py
+++ android_heater.py
@@ -3,12 +3,14 @@ import os
 import sys
 import subprocess
 
-from script import activated_file, writeValueToFile, val_required_temp_file
+from script import activated_file, writeValueToFile, val_required_temp_file,val_remaining_refresh_time_file
 
 # sys.argv[1] = temp   sys.argv[2] = activated
 # update temp_file with temp
 writeValueToFile(val_required_temp_file,sys.argv[1])
 print "Writed to %s value %s" %(val_required_temp_file,sys.argv[1]) 
+writeValueToFile(val_remaining_refresh_time_file,0)
+print "Restart counter of %s at 0" %val_remaining_refresh_time_file
 # start : set_activated_heater.py activated
 if sys.argv[2]=='False' or sys.argv[2]=='false' or sys.argv[2]=='True' or sys.argv[2]=='true' :
 	subprocess.call("python set_activated_heater.py %s" %sys.argv[2], shell=True)
