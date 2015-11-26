2331f2d546f183e096809007ab3b87a5878847e1
Valentin C <valentindu64@hotmail.fr>
2015-11-26 21:56:52 +0100

    Error : can't call subprocess from php code

--- android_heater.py
+++ android_heater.py
@@ -17,8 +17,16 @@ writeValueToFile(val_remaining_refresh_time_file,0)
 print "Restart counter of %s at 0 \n" %val_remaining_refresh_time_file
 # start : set_activated_heater.py activated
 if sys.argv[2]=='False' or sys.argv[2]=='false' or sys.argv[2]=='True' or sys.argv[2]=='true' :
-	subprocess.call("python set_activated_heater.py %s" %sys.argv[2], shell=True)
 	print "Set_activated_heater at %s \n" %sys.argv[2]
+
+	writeValueToFile(activated_file,sys.argv[2])
+	print "Write value %s to %s \n" %(sys.argv[2],activated_file)
+	if sys.argv[2]=='False' or sys.argv[2]=='false' :
+		print "Turn off heater \n" 
+		subprocess.call(action_command_turn_off, shell=True)
+		print "Write False to the %s\n" %is_on_heater
+		writeValueToFile(is_on_heater,'False')
+
 else:
 	print "Error with parameter activated at : %s \n" %sys.argv[2]
 print"\n"
