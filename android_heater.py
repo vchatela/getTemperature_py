2f850c6d207d8d8150796a4ff9571e335a9095f3
Valentin C <valentindu64@hotmail.fr>
2015-11-26 21:25:34 +0100

    More info to looger

--- android_heater.py
+++ android_heater.py
@@ -1,19 +1,23 @@
-#!/usr/bin/python
+﻿#!/usr/bin/python
 import os
 import sys
 import subprocess
 
 from script import activated_file, writeValueToFile, val_required_temp_file,val_remaining_refresh_time_file
 
+print "#####################\n"
+print "# ANDROID_HEATER.py #\n"
+print "#####################\n"
+
 # sys.argv[1] = temp   sys.argv[2] = activated
 # update temp_file with temp
 writeValueToFile(val_required_temp_file,sys.argv[1])
-print "Writed to %s value %s" %(val_required_temp_file,sys.argv[1]) 
+print "Writed to %s value %s \n" %(val_required_temp_file,sys.argv[1]) 
 writeValueToFile(val_remaining_refresh_time_file,0)
-print "Restart counter of %s at 0" %val_remaining_refresh_time_file
+print "Restart counter of %s at 0 \n" %val_remaining_refresh_time_file
 # start : set_activated_heater.py activated
 if sys.argv[2]=='False' or sys.argv[2]=='false' or sys.argv[2]=='True' or sys.argv[2]=='true' :
 	subprocess.call("python set_activated_heater.py %s" %sys.argv[2], shell=True)
-	print "Set_activated_heater at %s " %sys.argv[2]
+	print "Set_activated_heater at %s \n" %sys.argv[2]
 else:
-	print "Error with parameter activated at : %s" %sys.argv[2]
+	print "Error with parameter activated at : %s \n" %sys.argv[2]
--- set_activated_heater.py
+++ set_activated_heater.py
@@ -3,15 +3,20 @@ import os
 import sys
 import subprocess
 
-from script import activated_file, writeValueToFile, wiring_pin_rpi, action_command_turn_off
+from script import activated_file, writeValueToFile, wiring_pin_rpi, action_command_turn_off, dir_temp_file
 
-dir_temp_file = '/var/www/files'
 is_on_heater = dir_temp_file + '/is_on_heater'
 
+print "###########################\n"
+print "# SET_ACTIVATED_HEATER.py #\n"
+print "###########################\n"
+
+
 writeValueToFile(activated_file,sys.argv[1])
-print "Activated = %s" %sys.argv[1]
+print "Write value %s to %s \n" %(sys.argv[1],activated_file)
 if sys.argv[1]=='False' or sys.argv[1]=='false' :
-	print "turn off heater" 
+	print "Turn off heater \n" 
 	subprocess.call(action_command_turn_off, shell=True)
-	print "Write False to the is_on_heater file"
+	print "Write False to the %s" %is_on_heater
 	writeValueToFile(is_on_heater,'False')
+	print"\n\n\n"
