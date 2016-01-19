fc2bed17ebe4e7fb8bdde5be59cadf4be9aa1f5f
Valentin C <valentindu64@hotmail.fr>
2016-01-19 21:13:57 +0100

    def main and turn functions

--- android_heater.py
+++ android_heater.py
@@ -3,34 +3,46 @@ import os
 import sys
 import subprocess
 
-from script import activated_file, writeValueToFile, val_required_temp_file,val_remaining_refresh_time_file, turnOffHeater, turnOnHeater, read_temp
+from script import activated_file, writeValueToFile, val_required_temp_file,val_remaining_refresh_time_file, read_temp
 
-# sys.argv[1] = temp   sys.argv[2] = activated
-# update temp_file with temp
-print "Android_Heater.py \n"
-writeValueToFile(val_required_temp_file,sys.argv[1])
-print "Writed to %s value %s \n" %(val_required_temp_file,sys.argv[1]) 
-writeValueToFile(val_remaining_refresh_time_file,0)
-print "Restart counter of %s at 0 \n" %val_remaining_refresh_time_file
-# start : set_activated_heater.py activated
-if sys.argv[2].lower()==('false' or 'true'):
-	print "Set_activated_heater at %s \n" %sys.argv[2]
+def turnOnHeater():
+	print "turn on heater \n"
+	subprocess.call(action_command_turn_on, shell=True)
 
-	writeValueToFile(activated_file,sys.argv[2])
-	print "Write value %s to %s \n" %(sys.argv[2],activated_file)
-	if sys.argv[2].lower()=='false' :
-		turnOffHeater()
-		print "Write False to the %s \n" %is_on_heater
-	else : 
-		print "Test if need to turn on heater \n"
-		temp = float(read_temp())
-		# Get val_temp_requiered in file
-		val_temp_required = float(getValueFromFile(val_required_temp_file))
-		if temp < val_temp_required:
-			#turn on heater
-			turnOnHeater()
-		else:
-			#turn off heater
+def turnOffHeater():
+	print "turn off heater \n"
+	subprocess.call(action_command_turn_off, shell=True)
+
+def main():
+	# sys.argv[1] = temp   sys.argv[2] = activated
+	# update temp_file with temp
+	print "Android_Heater.py \n"
+	writeValueToFile(val_required_temp_file,sys.argv[1])
+	print "Writed to %s value %s \n" %(val_required_temp_file,sys.argv[1]) 
+	writeValueToFile(val_remaining_refresh_time_file,0)
+	print "Restart counter of %s at 0 \n" %val_remaining_refresh_time_file
+	# start : set_activated_heater.py activated
+	if sys.argv[2].lower()==('false' or 'true'):
+		print "Set_activated_heater at %s \n" %sys.argv[2]
+
+		writeValueToFile(activated_file,sys.argv[2])
+		print "Write value %s to %s \n" %(sys.argv[2],activated_file)
+		if sys.argv[2].lower()=='false' :
 			turnOffHeater()
-else:
-	print "Error with parameter activated at : %s \n" %sys.argv[2]
+			print "Write False to the %s \n" %is_on_heater
+		else : 
+			print "Test if need to turn on heater \n"
+			temp = float(read_temp())
+			# Get val_temp_requiered in file
+			val_temp_required = float(getValueFromFile(val_required_temp_file))
+			if temp < val_temp_required:
+				#turn on heater
+				turnOnHeater()
+			else:
+				#turn off heater
+				turnOffHeater()
+	else:
+		print "Error with parameter activated at : %s \n" %sys.argv[2]
+
+if __name__ == "__main__":
+	main()
