0ca085e8169b7df233a5c041961f741d79db42ca
Valentin C <valentindu64@hotmail.fr>
2016-04-07 13:23:54 +0200

    Added feature for GCM Android when sensor disconnected

--- script.py
+++ script.py
@@ -12,6 +12,7 @@ global password
 os.system('modprobe w1-gpio')
 os.system('modprobe w1-therm')
 base_dir = '/sys/bus/w1/devices/'
+nb_device = len(glob.glob(base_dir + '28*'))
 device_folder = glob.glob(base_dir + '28*')[0]
 device_file = device_folder + '/w1_slave'
 
@@ -28,8 +29,10 @@ val_remaining_add_db_file = dir_temp_file + '/val_remaining_add_db_file'
 wiring_pin_rpi = 29
 action_command_turn_on = '/var/www/hcc/radioEmission '+str(wiring_pin_rpi)+' 12325261 1 on'
 action_command_turn_off = '/var/www/hcc/radioEmission '+str(wiring_pin_rpi)+' 12325261 1 off'
-
-
+action_command_send = 'php gcm.php '
+message_chauffage = 'Chauffage toujours allumé !'
+message_capteur = 'Erreur du capteur'
+	
 def read_temp_raw():
 	f = open(device_file, 'r')
 	lines = f.readlines()
@@ -88,46 +91,52 @@ def turnOffHeater():
 ############################################################
 
 def main():
-	#If the heater it should not be activated - turn off (activated_heater off)
-	if getValueFromFile(activated_file).lower()=="false":
-		turnOffHeater()
-	# Get Temperature and store it each 20 minutes
-	temp = float(read_temp())
-	print "Current temperature : %f \n" %temp
-
-	remaining_time = float(getValueFromFile(val_remaining_add_db_file))
-	if(remaining_time==0):
-		add_temp_to_db(temp)
-		writeValueToFile(val_remaining_add_db_file,refresh_time)
-		print "Updated remaining time DB : %d \n" %int(refresh_time)
+	if nb_device=0:
+		#send notifcation
+		proc = subprocess.Popen(action_command_send + message_capteur, shell=True, stdout=subprocess.PIPE)
+		script_response = proc.stdout.read()
+		print script_response
 	else:
-		writeValueToFile(val_remaining_add_db_file,remaining_time-1)
-		print "Updated remaining time DB : %d \n" %(int(remaining_time)-1)
+		#If the heater it should not be activated - turn off (activated_heater off)
+		if getValueFromFile(activated_file).lower()=="false":
+			turnOffHeater()
+		# Get Temperature and store it each 20 minutes
+		temp = float(read_temp())
+		print "Current temperature : %f \n" %temp
 
-	remaining_time_heater = float(getValueFromFile(val_remaining_refresh_time_file))
-	if(remaining_time_heater==0):
-		# Get the activated boolean in file
-		s_activated = getValueFromFile(activated_file)
-		if s_activated.lower()=='true':
-			b_activated = True
+		remaining_time = float(getValueFromFile(val_remaining_add_db_file))
+		if(remaining_time==0):
+			add_temp_to_db(temp)
+			writeValueToFile(val_remaining_add_db_file,refresh_time)
+			print "Updated remaining time DB : %d \n" %int(refresh_time)
 		else:
-			b_activated = False
-		print "Activated : %s \n" %(str(b_activated))
-		# If activated check if heater is needed
-		if b_activated:
-		# Get val_temp_requiered in file
-			val_temp_required = float(getValueFromFile(val_required_temp_file))
-			if temp < val_temp_required:
-				#turn on heater
-				turnOnHeater()
+			writeValueToFile(val_remaining_add_db_file,remaining_time-1)
+			print "Updated remaining time DB : %d \n" %(int(remaining_time)-1)
+
+		remaining_time_heater = float(getValueFromFile(val_remaining_refresh_time_file))
+		if(remaining_time_heater==0):
+			# Get the activated boolean in file
+			s_activated = getValueFromFile(activated_file)
+			if s_activated.lower()=='true':
+				b_activated = True
 			else:
-				#turn off heater
-				turnOffHeater()
-		writeValueToFile(val_remaining_refresh_time_file,refresh_time_heater)
-		print "Updated remaining time heater %d \n" %int(refresh_time_heater)
-	else:
-		writeValueToFile(val_remaining_refresh_time_file,remaining_time_heater-1)
-		print "Updated remaining time heater : %d \n" %(int(remaining_time_heater)-1)
+				b_activated = False
+			print "Activated : %s \n" %(str(b_activated))
+			# If activated check if heater is needed
+			if b_activated:
+			# Get val_temp_requiered in file
+				val_temp_required = float(getValueFromFile(val_required_temp_file))
+				if temp < val_temp_required:
+					#turn on heater
+					turnOnHeater()
+				else:
+					#turn off heater
+					turnOffHeater()
+			writeValueToFile(val_remaining_refresh_time_file,refresh_time_heater)
+			print "Updated remaining time heater %d \n" %int(refresh_time_heater)
+		else:
+			writeValueToFile(val_remaining_refresh_time_file,remaining_time_heater-1)
+			print "Updated remaining time heater : %d \n" %(int(remaining_time_heater)-1)
 
 if __name__ == "__main__":
 	main()
